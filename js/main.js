bad7edd85d36bbc68b5c110947fefcf7010a8273
Paul <pkerschieter@gmail.com>
2024-09-10 21:05:54 -0500

    added Cyl conversion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*--------- app's constant (variables) ---------*/
 const CYLBYTE = 849960
+const TRKBYTE = 56664 
 const BYTECON  = 1024
 
 /*------ cached element references ------*/
@@ -9,25 +10,40 @@ const KBel = document.getElementById('KBel')
 const MBel = document.getElementById('MBel')
 const GBel = document.getElementById('GBel')
 const TBel = document.getElementById('TBel')
+const CTrkel = document.getElementById('CTrkel')
+const CBYTESel = document.getElementById('CBYTESel')
+const CKBel = document.getElementById('CKBel')
+const CMBel = document.getElementById('CMBel')
+const CGBel = document.getElementById('CGBel')
+const CTBel = document.getElementById('CTBel')
 
 /*--------- app's state (variables) ---------*/
 
+let trk
 let cly
 let bytes
 let kb
 let mb
 let gb
 let tb
+let cbytes
+let ckb
+let cmb
+let cgb
+let ctb
+
+
 
 /*--------- functions ---------*/
 
 function init() {
-  convert()
+  tconvert()
+  cconvert()
 }
 
 init()
 
-function convert() {
+function tconvert() {
   let tracks = (document.getElementById("tracks").value).replace(/,/g, '')
   console.log(tracks)
   cly = tracks / 15
@@ -44,6 +60,25 @@ function convert() {
   TBel.innerText= tb.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' tb'
 }
 
+
+function cconvert() {
+  let cylinder = (document.getElementById("cylinder").value).replace(/,/g, '')
+  console.log(tracks)
+  trk = cylinder * 15
+  cbytes = trk * TRKBYTE
+  ckb = cbytes / BYTECON
+  cmb = ckb / BYTECON
+  cgb = cmb / BYTECON
+  ctb = cgb / BYTECON
+  CTrkel.innerText= trk.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' tracks'
+  CBYTESel.innerText= cbytes.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' bytes'
+  CKBel.innerText= ckb.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' kb'
+  CMBel.innerText= cmb.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' mb'
+  CGBel.innerText= cgb.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' gb'
+  CTBel.innerText= ctb.toFixed(3).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + ' tb'
+}
+
 /*------ event listeners ------*/
-document.querySelector('#tracks').addEventListener('input', convert)
+document.querySelector('#tracks').addEventListener('input', tconvert)
+document.querySelector('#cylinder').addEventListener('input', cconvert)
 
